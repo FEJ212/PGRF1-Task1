eb6a125613a03e8d0530d037f0d40ab654b59819
fejteon1 <fejteon1@uhk.cz>
2024-11-13 11:56:06 +0100

    Hopefully completed pentagon calculation

--- src/model/Pentagon.java
+++ src/model/Pentagon.java
@@ -3,17 +3,25 @@ package model;
 public class Pentagon extends Polygon{
     private Polygon polygon = new Polygon();
     public Polygon calculate(Point start, Point end) {
+        int firstAngle = 54, secondAngle = 108, firstOppositeAngle = 180-90-firstAngle, secondOppositeAngle = 180-90-secondAngle;
         float r = (float) Math.sqrt((end.getX() - start.getX()) ^ 2 + (end.getY() - start.getY()) ^ 2);
         float a = (float) (r / ((Math.sqrt(50 + 10 * (Math.sqrt(5)))) / 10));
-        int firstAngle = 54, secondAngle = 108, firstOppositeAngle = 180-90-firstAngle, secondOppositeAngle = 180-90-secondAngle;
-
-        int firstX = ;
-        int firstY = ;
+        float b,c,tmpX,tmpY;
+        c = (float) (Math.sin(firstAngle)*a);
+        b = (float) ((c/Math.sin(firstAngle))*Math.sin(firstAngle));
+        int firstX = (int) (end.getX()+b);
+        int firstY = (int) (end.getY()+c);
         polygon.addPoint(new Point(firstX, firstY));
+        tmpX = end.getX();
+        tmpY = end.getY();
         for (int i = 0; i < 5; i++) {
-            int secondX = ;
-            int secondY = ;
+            c = (float) (Math.sin(firstAngle)*a);
+            b = (float) ((c/Math.sin(firstAngle))*Math.sin(firstAngle));
+            int secondX = (int) (tmpX+b);
+            int secondY = (int) (tmpY+c);
             polygon.addPoint(new Point(secondX, secondY));
+            tmpX = secondX;
+            tmpY = secondY;
         }
         return polygon;
     }
